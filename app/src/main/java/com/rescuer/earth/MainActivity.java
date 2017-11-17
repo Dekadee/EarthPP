5dfd900bdccb5745b60b1e31497a7c99b2431644
Dekadee <dominic.ebner@outlook.com>
2017-11-17 20:22:49 +0100

    First commit

---
+++ app/src/main/java/com/rescuer/earth/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rescuer.earth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startUp(View view){
+        Intent intent = new Intent(this,Navigator.class);
+        startActivity(intent);
+    }
+}
